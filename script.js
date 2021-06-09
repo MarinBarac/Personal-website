3a0f1db56ccce40b05ca89176bb96ec1f16f64be
lovricavericafesb <marin.barac.00@fesb.hr>
2021-06-09 21:08:07 +0200

    Added new header and mobile-nav animation

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-var toggleButton = document.querySelector('.toggle-button');
-var mobileNav = document.querySelector('.mobile-nav')
-var toggleButtonBar_1 = document.querySelector('.toggle-button__bar-1');
-var toggleButtonBar_3 = document.querySelector('.toggle-button__bar-3');
-var a;
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav')
+const toggleButtonBar_1 = document.querySelector('.toggle-button__bar-1');
+const toggleButtonBar_3 = document.querySelector('.toggle-button__bar-3');
+let a;
+const header = document.querySelector('header');
 
 toggleButton.addEventListener('click', function(){
     if(a == 1){
-        mobileNav.style.display = "none";
+        //mobileNav.style.display = "none";
         toggleButtonBar_1.style.animation = "up-1 500ms forwards";
         toggleButtonBar_3.style.animation = "down-2 500ms forwards";
+        mobileNav.style.animation = "slide-right 500ms forwards";
         return a=0;
     }
     else {
-        mobileNav.style.display = "flex";
+        //mobileNav.style.display = "flex";
         toggleButtonBar_1.style.animation = "down-1 500ms forwards";
         toggleButtonBar_3.style.animation = "up-2 500ms forwards";
+        mobileNav.style.animation = "slide-left 500ms forwards";
         return a=1;
     }
 })
@@ -23,8 +26,18 @@ toggleButton.addEventListener('click', function(){
 mobileNav.addEventListener('click', closeMobileNav)
 
 function closeMobileNav() {
-    mobileNav.style.display = "none";
+    //mobileNav.style.display = "none";
     toggleButtonBar_1.style.animation = "up-1 500ms forwards";
     toggleButtonBar_3.style.animation = "down-2 500ms forwards";
+    mobileNav.style.animation = "slide-right 500ms forwards";
     return a=0;
 }
+
+document.addEventListener('scroll', () => {
+	let scrollPosition = window.scrollY;
+	if (scrollPosition > 150) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'rgba(0,0,0,0.1)';
+	}
+});
